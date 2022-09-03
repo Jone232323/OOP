b3fcec2307770d05eff6e85e31cdd1b2d6716cd1
Jone23 <jone23@rambler.ru>
2022-09-03 08:55:32 +0300

    add two new object myBox2 and myBox3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,23 @@ public class Main {
         myBox.height = 5.7;
         myBox.length = 7.0;
 
-        double volume =   myBox.width* myBox.height*myBox.length;
+        Box myBox2 = new Box();
+        myBox2.width = 5;
+        myBox2.height = 5;
+        myBox2.length = 5;
+
+        Box myBox3 = new Box();
+        myBox3.width = 10;
+        myBox3.height = 10;
+        myBox3.length = 10;
+
+        double volume = myBox.width * myBox.height * myBox.length;
+
+        double volume2 = myBox2.width * myBox2.height * myBox2.length;
+
+        double volume3 = myBox3.width * myBox3.height * myBox3.length;
         System.out.println(volume);
+        System.out.println(volume2);
+        System.out.println(volume3);
     }
 }
