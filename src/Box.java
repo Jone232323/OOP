9d059d84e368156cf0c41d923e5de77bfb262020
Jone23 <jone23@rambler.ru>
2022-09-03 11:30:00 +0300

    add metode  showVolume

--- src/Box.java
+++ src/Box.java
@@ -2,4 +2,9 @@ public class Box {
     double width;
     double height;
     double length;
+
+    public void showVolume(){
+        double volume = width * height * length;
+        System.out.println(volume);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Box myBox = new Box();
-        myBox.width = 3.3;
-        myBox.height = 5.7;
-        myBox.length = 7.0;
 
         Box myBox2 = new Box();
         myBox2.width = 5;
         myBox2.height = 5;
         myBox2.length = 5;
+        myBox2.showVolume();
+
 
         Box myBox3 = new Box();
         myBox3.width = 10;
         myBox3.height = 10;
         myBox3.length = 10;
+        myBox3.showVolume();
 
-        double volume = myBox.width * myBox.height * myBox.length;
 
-        double volume2 = myBox2.width * myBox2.height * myBox2.length;
-
-        double volume3 = myBox3.width * myBox3.height * myBox3.length;
-        System.out.println(volume);
-        System.out.println(volume2);
-        System.out.println(volume3);
     }
 }
