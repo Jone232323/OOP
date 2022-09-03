ffdb17ccbad9dfb3ebce2c3f9b7bcb4656266d39
Jone23 <jone23@rambler.ru>
2022-09-03 08:50:52 +0300

    add new class Box and create new object myBox

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Box myBox = new Box();
+        myBox.width = 3.3;
+        myBox.height = 5.7;
+        myBox.length = 7.0;
+
+        double volume =   myBox.width* myBox.height*myBox.length;
+        System.out.println(volume);
     }
 }
